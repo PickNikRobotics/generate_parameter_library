9c6e262af66bd17470f4e8c353f7228c26d1d6f9
Paul Gesel <paul.gesel@picknik.ai>
2022-06-23 21:30:04 -0600

    avoid strinf copies with buffer and remove += str concats

--- gen_param_struct/gen_param_struct.py
+++ gen_param_struct/gen_param_struct.py
@@ -4,14 +4,28 @@ import yaml
 import sys
 import os
 
+
+# class to help minimize string copies
+class Buffer:
+    def __init__(self):
+        self.data_ = bytearray()
+
+    def __iadd__(self, element):
+        self.data_.extend(element.encode())
+        return self
+
+    def __str__(self):
+        return self.data_.decode()
+
+
 # class used to fill template.txt using passed in yaml file
 class GenParamStruct:
 
     def __init__(self):
         self.contents = ""
-        self.struct = ""
-        self.param_set = ""
-        self.param_declare = ""
+        self.struct = Buffer()
+        self.param_set = Buffer()
+        self.param_declare = Buffer()
         self.target = ""
 
     def parse_params(self, name, value, nested_name_list):
@@ -19,41 +33,37 @@ class GenParamStruct:
         nested_name = "".join(x + "_." for x in nested_name_list[1:])
 
         if isinstance(value, list) and isinstance(value[0], str):
-            self.struct += "std::vector<std::string> " + name +" = {"
-            for ind, val in enumerate(value):
-                if ind < len(value)-1:
-                    self.struct += "\"" + val + "\"" + ", "
-                else:
-                    self.struct += "\"" + val + "\""  + "};\n"
+            self.struct += "std::vector<std::string> %s = {" % name
+            for ind, val in enumerate(value[:-1]):
+                self.struct += "\"%s\", " % val
+            self.struct += "\"%s\"};\n" % value[-1]
 
-            self.param_set += "if (param.get_name() == " + "\"" + name + "\") {\n"
-            self.param_set += nested_name + name + " = param.as_string_array();\n"
+            self.param_set += "if (param.get_name() == " + "\"%s\") {\n" % name
+            self.param_set += "%s = param.as_string_array();\n" % (nested_name + name)
             self.param_set += "}\n"
 
         elif isinstance(value, list) and isinstance(value[0], float):
-            self.struct += "std::vector<double> " + name +" = {"
-            for ind, val in enumerate(value):
-                if ind < len(value)-1:
-                    self.struct += str(val) + ", "
-                else:
-                    self.struct += str(val) + "};\n"
+            self.struct += "std::vector<double> %s = {" % name
+            for ind, val in enumerate(value[:-1]):
+                self.struct += "\"%s\", " % val
+            self.struct += "\"%s\"};\n" % value[-1]
 
-            self.param_set += "if (param.get_name() == " + "\"" + name + "\") {\n"
-            self.param_set += nested_name + name + " = param.as_double_array();\n"
+            self.param_set += "if (param.get_name() == " + "\"%s\") {\n" % name
+            self.param_set += "%s = param.as_double_array();\n" % (nested_name + name)
             self.param_set += "}\n"
 
         elif isinstance(value, str):
-            self.struct += "std::string " + name + " = \"" + value + "\";\n"
+            self.struct += "std::string %s = \"%s\";\n" % (name, value)
 
-            self.param_set += "if (param.get_name() == " + "\"" + name + "\") {\n"
-            self.param_set += nested_name + name + " = param.as_string();\n"
+            self.param_set += "if (param.get_name() == " + "\"%s\") {\n" % name
+            self.param_set += "%s = param.as_string();\n" % (nested_name + name)
             self.param_set += "}\n"
 
         elif isinstance(value, float):
-            self.struct += "double " + name + " = " + str(value)+";\n"
+            self.struct += "double %s = %s;\n" % (name, str(value))
 
-            self.param_set += "if (param.get_name() == " + "\"" + name + "\") {\n"
-            self.param_set += nested_name + name + " = param.as_double();\n"
+            self.param_set += "if (param.get_name() == " + "\"%s\") {\n" % name
+            self.param_set += "%s = param.as_double();\n" % (nested_name + name)
             self.param_set += "}\n"
 
         elif isinstance(value, bool):
@@ -61,29 +71,31 @@ class GenParamStruct:
                 tmp = "true"
             else:
                 tmp = "false"
-            self.struct += "bool " + name + " = " + tmp + ";\n"
+            self.struct += "bool %s = %s;\n" % (name, tmp)
 
-            self.param_set += "if (param.get_name() == " + "\"" + name + "\") {\n"
-            self.param_set += nested_name + name + " = param.as_bool();\n"
+            self.param_set += "if (param.get_name() == " + "\"%s\") {\n" % name
+            self.param_set += "%s = param.as_bool();\n" % (nested_name + name)
             self.param_set += "}\n"
 
-        param_prefix = "p_" + "".join(x + "_" for x in nested_name_list[1:])
+        param_prefix = "p_"
+        param_prefix += "".join(x + "_" for x in nested_name_list[1:])
         param_name = "".join(x + "." for x in nested_name_list[1:]) + name
 
-        self.param_declare += "auto " + param_prefix + name + " = rclcpp::ParameterValue(" + nested_name + name + ");\n"
-        self.param_declare += "parameters_interface->declare_parameter(\"" + param_name + "\", " + param_prefix + name +");\n"
+        self.param_declare += "auto %s = rclcpp::ParameterValue(%s);\n" % (param_prefix + name, nested_name + name)
+        self.param_declare += "parameters_interface->declare_parameter(\"%s\", %s);\n" % (
+            param_name, param_prefix + name)
 
     def parse_dict(self, name, root_map, nested_name):
         if isinstance(root_map, dict):
             if name != self.target:
-                self.struct += "struct "+name+" {\n"
+                self.struct += "struct %s {\n" % name
             for key in root_map:
                 nested_name.append(name)
                 self.parse_dict(key, root_map[key], nested_name)
                 nested_name.pop()
 
             if name != self.target:
-                self.struct += "} " + name + "_;\n"
+                self.struct += "} %s_;\n" % name
         else:
             self.parse_params(name, root_map, nested_name)
 
@@ -113,7 +125,6 @@ class GenParamStruct:
                     if k == self.target:
                         self.parse_dict(self.target, v['ros__parameters'], [])
 
-
         COMMENTS = "// this is auto-generated code "
         INCLUDES = "#include <rclcpp/node.hpp>\n#include <vector>\n#include <string>"
         NAMESPACE = self.target + "_parameters"
@@ -124,19 +135,17 @@ class GenParamStruct:
         self.contents = self.contents.replace("**COMMENTS**", COMMENTS)
         self.contents = self.contents.replace("**INCLUDES**", INCLUDES)
         self.contents = self.contents.replace("**NAMESPACE**", NAMESPACE)
-        self.contents = self.contents.replace("**STRUCT_NAME**", self.target)
-        self.contents = self.contents.replace("**STRUCT_CONTENT**", self.struct)
-        self.contents = self.contents.replace("**PARAM_SET**", self.param_set)
-        self.contents = self.contents.replace("**DECLARE_PARAMS**", self.param_declare)
-
+        self.contents = self.contents.replace("**STRUCT_NAME**", str(self.target))
+        self.contents = self.contents.replace("**STRUCT_CONTENT**", str(self.struct))
+        self.contents = self.contents.replace("**PARAM_SET**", str(self.param_set))
+        self.contents = self.contents.replace("**DECLARE_PARAMS**", str(self.param_declare))
 
         if not os.path.isdir(out_directory):
             print("The specified output directory: " + out_directory + " does not exist")
             raise AssertionError()
 
-        with open(out_directory + self.target  + ".h", "w") as f:
+        with open(out_directory + self.target + ".h", "w") as f:
             f.write(self.contents)
-
 
 
 if __name__ == "__main__":
