df4ed3ef25f9a52139c138425a73fdf50fbd07e9
Tyler Weaver <tyler@picknik.ai>
2022-07-22 09:37:29 -0600

    fmt for formatting errors
    Signed-off-by: Tyler Weaver <tyler@picknik.ai>

--- generate_parameter_library_py/generate_parameter_library_py/cpp_templates/validators.hpp
+++ generate_parameter_library_py/generate_parameter_library_py/cpp_templates/validators.hpp
@@ -2,16 +2,7 @@ class Result {
  public:
   template <typename... Args>
   Result(const std::string& format, Args... args) {
-    int size_s = std::snprintf(nullptr, 0, format.c_str(), args...) +
-                 1;  // Extra space for '\0'
-    if (size_s <= 0) {
-      throw std::runtime_error("Error during formatting.");
-    }
-    auto size = static_cast<size_t>(size_s);
-    std::unique_ptr<char[]> buf(new char[size]);
-    std::snprintf(buf.get(), size, format.c_str(), args...);
-    msg_ = std::string(buf.get(),
-                       buf.get() + size - 1);  // We don't want the '\0' inside
+    msg_ = fmt::format(format, args...);
     success_ = false;
   }
 
@@ -33,7 +24,7 @@ Result validate_string_array_len(const rclcpp::Parameter& parameter,
                                  size_t size) {
   const auto& string_array = parameter.as_string_array();
   if (string_array.size() != size) {
-    return ERROR("Invalid length '%d' for parameter %s. Required length: %d",
+    return ERROR("Invalid length '{}' for parameter {}. Required length: {}",
                  string_array.size(), parameter.get_name().c_str(), size);
   }
   return OK;
@@ -43,7 +34,7 @@ Result validate_double_array_len(const rclcpp::Parameter& parameter,
                                  size_t size) {
   const auto& double_array = parameter.as_double_array();
   if (double_array.size() != size) {
-    return ERROR("Invalid length '%d' for parameter %s. Required length: %d",
+    return ERROR("Invalid length '{}' for parameter {}. Required length: {}",
                  double_array.size(), parameter.get_name().c_str(), size);
   }
   return OK;
@@ -53,7 +44,7 @@ Result validate_bool_array_len(const rclcpp::Parameter& parameter,
                                size_t size) {
   const auto& bool_array = parameter.as_bool_array();
   if (bool_array.size() != size) {
-    return ERROR("Invalid length '%d' for parameter %s. Required length: %d",
+    return ERROR("Invalid length '{}' for parameter {}. Required length: {}",
                  bool_array.size(), parameter.get_name().c_str(), size);
   }
   return OK;
@@ -65,7 +56,7 @@ Result validate_double_array_bounds(const rclcpp::Parameter& parameter,
   for (auto val : double_array) {
     if (val < lower_bound || val > upper_bound) {
       return ERROR(
-          "Invalid value '%f' for parameter %s. Required bounds: [%f, %f]", val,
+          "Invalid value '{}' for parameter {}. Required bounds: [{}, {}]", val,
           parameter.get_name().c_str(), lower_bound, upper_bound);
     }
   }
@@ -78,7 +69,7 @@ Result validate_int_array_bounds(const rclcpp::Parameter& parameter,
   for (auto val : integer_array) {
     if (val < lower_bound || val > upper_bound) {
       return ERROR(
-          "Invalid value '%d' for parameter %s. Required bounds: [%d, %d]", val,
+          "Invalid value '%d' for parameter {}. Required bounds: [%d, %d]", val,
           parameter.get_name().c_str(), lower_bound, upper_bound);
     }
   }
@@ -93,9 +84,8 @@ Result validate_one_of(rclcpp::Parameter const& parameter,
   if (collection.find(param_value) == collection.end()) {
     std::stringstream ss;
     for (auto const& c : collection) ss << c << ", ";
-    return ERROR("The parameter (%s) with the value (%s) not in the set: [%s]",
-                 parameter.get_name(), std::string{param_value},
-                 ss.str().c_str());
+    return ERROR("The parameter ({}) with the value ({}) not in the set: [{}]",
+                 parameter.get_name(), param_value, ss.str().c_str());
   }
 
   return OK;
