ea84f8050e7fb189571e085f69e028959c1ba57b
Christoph Froehlich <christoph.froehlich@ait.ac.at>
2023-12-28 14:44:02 +0000

    Fix imported function names

--- generate_parameter_library_py/generate_parameter_library_py/test/YAML_parse_error_test.py
+++ generate_parameter_library_py/generate_parameter_library_py/test/YAML_parse_error_test.py
@@ -19,8 +19,8 @@ import sys
 import os
 
 from ament_index_python.packages import get_package_share_path
-from generate_parameter_library_py.generate_cpp_header import run as run_python
-from generate_parameter_library_py.generate_python_module import run as run_cpp
+from generate_parameter_library_py.generate_cpp_header import run as run_cpp
+from generate_parameter_library_py.generate_python_module import run as run_python
 from generate_parameter_library_py.generate_markdown import run as run_md
 from generate_parameter_library_py.parse_yaml import YAMLSyntaxError
 from generate_parameter_library_py.generate_cpp_header import parse_args
