635ad6c643762d16737d7704e1dc1f9512363ef3
Paul Gesel <paulgesel@gmail.com>
2024-01-01 13:02:26 -0500

    add print to python code gen
    Signed-off-by: Paul Gesel <paulgesel@gmail.com>

--- generate_parameter_library_py/generate_parameter_library_py/generate_python_module.py
+++ generate_parameter_library_py/generate_parameter_library_py/generate_python_module.py
@@ -37,6 +37,7 @@ from generate_parameter_library_py.parse_yaml import GenerateCode
 
 
 def run(output_file, yaml_file, validation_module=''):
+    print(f'Running {__file__} {output_file} {yaml_file} {validation_module}')
     gen_param_struct = GenerateCode('python')
     output_dir = os.path.dirname(output_file)
     if not os.path.isdir(output_dir):
@@ -62,7 +63,6 @@ def main():
     output_file = args.output_python_module_file
     yaml_file = args.input_yaml_file
     validate_file = args.validate_file
-
     run(output_file, yaml_file, validate_file)
 
 
