6f3bfe7d3347ece2574bb81047647c6176df1333
AugusteBourgois <auguste@forssea-robotics.fr>
2024-08-19 12:15:23 +0200

    correct cmake linter error

--- generate_parameter_library/cmake/generate_parameter_library.cmake
+++ generate_parameter_library/cmake/generate_parameter_library.cmake
@@ -71,15 +71,16 @@ macro(generate_parameter_library LIB_NAME YAML_FILE)
   # necessary so that #include <param_file.hpp> can be used in the local package (deprecated)
   set(LOCAL_PARAM_HEADER_FILE ${CMAKE_CURRENT_BINARY_DIR}/include/${LIB_NAME}.hpp)
   add_custom_command(
-      OUTPUT ${LOCAL_PARAM_HEADER_FILE}
-      COMMAND ${CMAKE_COMMAND} -E echo "#pragma message(\"#include \\\"${LIB_NAME}.hpp\\\" is deprecated. Use #include <${PROJECT_NAME}/${LIB_NAME}.hpp> instead.\")" >> ${LOCAL_PARAM_HEADER_FILE}
-      COMMAND ${CMAKE_COMMAND} -E cat ${LOCAL_PARAM_HEADER_FILE} ${PARAM_HEADER_FILE} > ${LOCAL_PARAM_HEADER_FILE}.tmp
-      COMMAND ${CMAKE_COMMAND} -E copy ${LOCAL_PARAM_HEADER_FILE}.tmp ${LOCAL_PARAM_HEADER_FILE}
-      COMMAND ${CMAKE_COMMAND} -E remove ${LOCAL_PARAM_HEADER_FILE}.tmp
-      DEPENDS ${PARAM_HEADER_FILE}
-      COMMENT
-      "Creating deprecated header file ${LOCAL_PARAM_HEADER_FILE}"
-      VERBATIM
+    OUTPUT ${LOCAL_PARAM_HEADER_FILE}
+    COMMAND ${CMAKE_COMMAND} -E echo "#pragma message(\"#include \\\"${LIB_NAME}.hpp\\\" is deprecated. \
+Use #include <${PROJECT_NAME}/${LIB_NAME}.hpp> instead.\")" >> ${LOCAL_PARAM_HEADER_FILE}
+    COMMAND ${CMAKE_COMMAND} -E cat ${LOCAL_PARAM_HEADER_FILE} ${PARAM_HEADER_FILE} > ${LOCAL_PARAM_HEADER_FILE}.tmp
+    COMMAND ${CMAKE_COMMAND} -E copy ${LOCAL_PARAM_HEADER_FILE}.tmp ${LOCAL_PARAM_HEADER_FILE}
+    COMMAND ${CMAKE_COMMAND} -E remove ${LOCAL_PARAM_HEADER_FILE}.tmp
+    DEPENDS ${PARAM_HEADER_FILE}
+    COMMENT
+    "Creating deprecated header file ${LOCAL_PARAM_HEADER_FILE}"
+    VERBATIM
   )
 
   # Create the library target
