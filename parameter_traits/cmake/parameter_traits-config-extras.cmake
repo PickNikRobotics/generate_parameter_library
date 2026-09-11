dc8d95ccfe61b6f2373f2255f0dc35ff13f1fde6
Christoph Froehlich <christoph.froehlich@ait.ac.at>
2026-09-11 22:42:07 +0000

    Silence CMake deprecation warning for transitive inclusions

---
+++ parameter_traits/cmake/parameter_traits-config-extras.cmake
@@ -0,0 +1,3 @@
+# Silence deprecation output from tl_expected when this package is consumed
+# transitively through generated ament export dependency CMake files.
+set(tl_expected_DEPRECATED_QUIET TRUE)
